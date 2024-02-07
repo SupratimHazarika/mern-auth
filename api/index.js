fef020924f1d20fa232a23e4682472db693f5086
Supratim Hazarika <supratimhazarika@Supratims-MacBook-Air.local>
2024-02-07 17:34:26 +0530

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('connected to mongodb')
+})
+.catch((err) => console.log(err))
 
 app.listen(3000, () => {
     console.log('server is listening to the port 3000')
